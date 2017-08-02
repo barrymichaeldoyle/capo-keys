24259e04cc9db14f47ddb8f4ce2095edce2eb4f8
Barry Michael Doyle <barrydoyle18@gmail.com>
2017-08-02 08:29:29 +0200

    Updated json files

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { StackNavigator } from 'react-navigation';
 
 import store from './store';
 import MainScreen from './screens/MainScreen';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     const MainNavigator = StackNavigator({
       Main: { screen: MainScreen }
