a338fb40af2c0c0db4fce7f558653d03c3b676cb
Barry Michael Doyle <barrydoyle18@gmail.com>
2017-08-02 09:00:57 +0200

    Created Banner layout (Banner not implemented yet)

--- components/BottomBannerAd.js
+++ components/BottomBannerAd.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
 import { FacebookAds } from 'expo';
 
-const BottomBannerAd = (props) => (
-  <View style={props.style}>
-    <FacebookAds.BannerView
-      placementId="328225604270934_328227207604107"
-      type="standard"
-      onPress={() => console.log('Banner ad clicked')}
-      onError={err => console.log('Banner Ad Error', err)}
-    />
-  </View>
+const BottomBannerAd = () => (
+  <FacebookAds.BannerView
+    placementId="328225604270934_328227207604107"
+    type="standard"
+    onPress={() => console.log('Banner ad clicked')}
+    onError={err => console.log('Banner Ad Error', err)}
+  />
 );
 
 export default BottomBannerAd;
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import { View, Platform, Image } from 'react-native';
+import { View, Platform, Image, Text } from 'react-native';
+import { Constants } from 'expo';
 import KeysButtons from '../components/KeysButtons';
 import CapoButtons from '../components/CapoButtons';
 import CapoKey from '../components/CapoKey';
+import BottomBannerAd from '../components/BottomBannerAd';
 import icon from '../assets/icons/pure-icon.png';
+import { SCREEN_WIDTH } from '../style_constants';
 
 class MainScreen extends Component {
   static navigationOptions = () => ({
     title: 'Capo Keys',
     headerStyle: {
-      height: Platform.OS === 'android' ? 78 : 54,
+      height: Platform.OS === 'android' ? 54 + Constants.statusBarHeight : 54,
       backgroundColor: '#2169F3'
     },
     headerTitleStyle: {
-      marginTop: Platform.OS === 'android' ? 24 : 0,
+      marginTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
       color: 'white'
     },
     headerLeft: (
@@ -30,11 +33,18 @@ class MainScreen extends Component {
   });
 
   render() {
+    const { containerStyle, adContainerStyle, buttonStyle } = styles;
+
     return (
-      <View style={styles.containerStyle}>
-        <KeysButtons />
-        <CapoButtons />
-        <CapoKey />
+      <View style={{ flex: 1 }}>
+        <View style={containerStyle}>
+          <KeysButtons />
+          <CapoButtons />
+          <CapoKey />
+        </View>
+        <View style={adContainerStyle}>
+          <Text>Banner add goes here</Text>
+        </View>
       </View>
     );
   }
@@ -44,7 +54,18 @@ const styles = {
   containerStyle: {
     flex: 1,
     justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#ddd'
+  },
+  adContainerStyle: {
+    height: 50,
+    backgroundColor: '#ccc',
+    width: SCREEN_WIDTH,
+    justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttonStyle: {
+    width: SCREEN_WIDTH * 0.9
   }
 };
 
