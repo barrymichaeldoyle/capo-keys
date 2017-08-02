32a13376300c250becc5bcdbfb0fd35c03aaf504
Barry Michael Doyle <barrydoyle18@gmail.com>
2017-08-02 13:18:37 +0200

    Implemented Interstitial ad

--- actions/modal_actions.js
+++ actions/modal_actions.js
@@ -1,3 +1,5 @@
+import { FacebookAds } from 'expo';
+
 import {
   OPEN_CHORDS_MODAL,
   CLOSE_CHORDS_MODAL
@@ -8,7 +10,9 @@ export const openChordsModal = () => ({
   payload: true
 });
 
-export const closeChordsModal = () => ({
-  type: CLOSE_CHORDS_MODAL,
-  payload: false
-});
+export const closeChordsModal = () => {
+  FacebookAds.InterstitialAdManager.showAd('328225604270934_328677554225739')
+    .then(console.log('Banner ad clicked'))
+    .catch(err => console.log('Interstitial Ad Error', err));
+  return { type: CLOSE_CHORDS_MODAL, payload: false };
+};
