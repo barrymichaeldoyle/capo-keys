0069a7016e0a3c9106c85215e9db4fc3ac614f34
Barry Michael Doyle <barrydoyle18@gmail.com>
2017-09-02 11:25:55 +0200

    small tweak

--- actions/modal_actions.js
+++ actions/modal_actions.js
@@ -10,9 +10,16 @@ export const openChordsModal = () => ({
   payload: true
 });
 
+let expirationDate;
+
 export const closeChordsModal = () => {
-  FacebookAds.InterstitialAdManager.showAd('328225604270934_328677554225739')
-    .then(console.log('Intestitial Ad shown'))
-    .catch(err => console.log('Interstitial Ad Error', err));
+  if (!expirationDate || new Date() > expirationDate) {
+    expirationDate = new Date(
+      new Date().getTime() + (2 * 60 * 1000)
+    );
+    FacebookAds.InterstitialAdManager.showAd('328225604270934_328677554225739')
+      .then(console.log('Intestitial Ad shown'))
+      .catch(err => console.log('Interstitial Ad Error', err));
+  }
   return { type: CLOSE_CHORDS_MODAL, payload: false };
 };
